9712594d0ebde657ca50cd5382d96d26ba8a88a2
Dominicksam <dominicsamakpan@gmail.com>
2023-08-11 20:27:00 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - ENtry point
+  *Description print: alphabets in lower case and followed by a line'
+  *Return: ALways 0
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	Return(0);
+}
