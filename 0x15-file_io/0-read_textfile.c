5c62c77911ab4623a5a9a1dd6ec10327efc92473
Dominicksam <dominicsamakpan@gmail.com>
2023-10-02 19:36:51 -0700

    Task 0 file i0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile- Read text file print to the POSIX STDOUT.
+ * @filename: text file to be read and print
+ * @letters: number of letters to be read and print
+ * Return: n- actual number of bytes read and printed
+ * 0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t fd;
+	ssize_t n;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	
+	buff = malloc(sizeof(char) * letters);
+	t = read(fd, buff, letters);
+	n = write(STDOUT_FILENO, buff, t);
+
+	free(buff);
+	close(fd);
+	return (n);
+}
+
