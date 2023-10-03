6b31313e2cee8988c3d7f71503b604f64a2460b9
Dominicksam <dominicsamakpan@gmail.com>
2023-10-02 20:39:37 -0700

    Task 1 file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,14 +7,14 @@
  * read_textfile- Read text file print to the POSIX STDOUT.
  * @filename: text file to be read and print
  * @letters: number of letters to be read and print
- * Return: n- actual number of bytes read and printed
+ * Return: nw- actual number of bytes read and printed
  * 0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buff;
 	ssize_t fd;
-	ssize_t n;
+	ssize_t nw;
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
@@ -23,10 +23,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buff = malloc(sizeof(char) * letters);
 	t = read(fd, buff, letters);
-	n = write(STDOUT_FILENO, buff, t);
+	nw = write(STDOUT_FILENO, buff, t);
 
 	free(buff);
 	close(fd);
-	return (n);
+	return (nw);
 }
 
