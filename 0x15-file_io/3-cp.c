879edba638032a9a301530103d982945c3bbc5ba
Dominicksam <dominicsamakpan@gmail.com>
2023-12-07 19:01:34 -0800

    Task 0

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,8 +12,8 @@
  *
  * Return: 0 on success, or an error code on failure.
  */
- 
-int main(int argc, char *argv[]) 
+
+int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
-		return 97;
+		return (97);
 	}
 
 	fd_from = open(argv[1], O_RDONLY);
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	if (fd_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		return 98;
+		return (98);
 	}
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
@@ -37,34 +37,34 @@ int main(int argc, char *argv[])
 	{
 		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
 		close(fd_from);
-		return 99;
+		return (99);
 	}
 
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
+		
 		{
 			dprintf(2, "Error: Can't write to file %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
-			return 99;
+			return (99);
 		}
 	}
 	if (bytes_read == -1)
 	{
-        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-        close(fd_from);
-        close(fd_to);
-        return 98;
-    }
-    
-    if (close(fd_from) == -1 || close(fd_to) == -1) 
-    {
-	dprintf(2, "Error: Can't close fd\n");
-	return 100;
-    }
-    return 0;
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		return (98);
+	}
+	
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd\n");
+		return (100);
+	}
+	return (0);
 }
-
 
