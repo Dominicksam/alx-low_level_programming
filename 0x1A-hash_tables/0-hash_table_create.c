6ea34b7171f06eed482da21ada9a7dcd75239ba2
Dominicksam <dominicsamakpan@gmail.com>
2023-12-22 06:21:56 -0800

    Task 0b

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,32 +11,28 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table;
-    unsigned long int i;
+	hash_table_t *hash_table;
+	unsigned long int i;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    /* Allocate memory for the hash table structure */
-    hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL)
-        return (NULL);
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
 
-    /* Allocate memory for the array of pointers (buckets) */
-    hash_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (hash_table->array == NULL)
-    {
-        free(hash_table);
-        return (NULL);
-    }
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
 
-    /* Initialize each bucket to NULL */
-    for (i = 0; i < size; i++)
-        hash_table->array[i] = NULL;
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
 
-    /* Set the size of the array */
-    hash_table->size = size;
+	hash_table->size = size;
 
-    return (hash_table);
+	return (hash_table);
 }
 
